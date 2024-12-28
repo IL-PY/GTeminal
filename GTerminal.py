6c296ec820035bb78b72d820cb76f4d357b532d5
IL-PY <illiarezerv4@gmail.com>
2024-12-28 14:45:41 +0200

    Update GTerminal.py

--- GTerminal.py
+++ GTerminal.py
@@ -3,13 +3,45 @@ import sys
 import time
 import webbrowser as wb
 
-os.system("title GTerminal")
+
+l = os.getlogin()
+os.system(f"title GTerminal/{l}")
 os.system("echo off")
 with open ("config.cnf", "r") as config:
     os.system("color " + config.readline())
     config.close()
 os.system("cls")
-l = os.getlogin()
+print("GTerminal is loaded")
+time.sleep(0.5)
+os.system('cls')
+print("G")
+time.sleep(0.1)
+os.system('cls')
+print("Go")
+time.sleep(0.1)
+os.system('cls')
+print("Goo")
+time.sleep(0.1)
+os.system('cls')
+print("Good")
+time.sleep(0.1)
+os.system('cls')
+
+print("Good l")
+time.sleep(0.1)
+os.system('cls')
+print("Good lu")
+time.sleep(0.1)
+os.system('cls')
+print("Good luc")
+time.sleep(0.1)
+os.system('cls')
+print("Good luck")
+time.sleep(2)
+os.system('cls')
+
+
+
 
 while True:
     cmd = input("$")
@@ -17,14 +49,12 @@ while True:
         sys.exit()
     elif cmd == "TW":
         wb.open_new("https://turbowarp.org/")
-    elif cmd == "Pack":
-        wb.open_new("")
+
     elif cmd == "cnf edit":
         with open("config.cnf", "w", encoding='utf-8') as cnf:
             r = cnf.write(input())
-    
     elif cmd == "config":
-        f = open('config.txt', 'r')
+        f = open('config.cnf', 'r')
         a = f.readline()
         f.close()
         print(a)
